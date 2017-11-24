7fe4bb3942fb7e60f57c15588b5095562edae7e8
tkerns1965 <tony.kerns.65@gmail.com>
2017-11-24 09:24:58 -0500

    Add files via upload

---
+++ ampache/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ ! -f /var/www/config/ampache.cfg.php ]]; then
+    mv /var/temp/ampache.cfg.php.dist /var/www/config/ampache.cfg.php.dist
+fi
+
+# Start apache in the background
+# service apache2 start
+
+# Start cron in the background
+cron
+
+# Start a process to watch for changes in the library with inotify
+(
+while true; do
+    inotifywatch /media
+    php /var/www/bin/catalog_update.inc -a
+    sleep 30
+done
+) &
+
+# run this in the foreground so Docker won't exit
+# exec mysqld_safe
+apachectl -DFOREGROUND
