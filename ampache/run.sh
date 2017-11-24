c5e28045bda44f19f2c1ec8efe0348f68bb8c666
tkerns1965 <tony.kerns.65@gmail.com>
2017-11-24 11:24:28 -0500

    Update run.sh

--- ampache/run.sh
+++ ampache/run.sh
@@ -4,7 +4,7 @@ if [[ ! -f /var/www/config/ampache.cfg.php ]]; then
     mv /var/temp/ampache.cfg.php.dist /var/www/config/ampache.cfg.php.dist
 fi
 
-chown -R www-data:www-data /var/www
+# chown -R www-data:www-data /var/www
 
 # Start apache in the background
 # service apache2 start
